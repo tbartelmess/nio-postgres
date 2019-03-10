ff8fde4a3895f534400ce6ed6a58a8e93e22887e
Thomas Bartelmess <tbartelmess@marketcircle.com>
2019-03-10 12:09:46 +0100

    Reverted PostgresMessageEncoder

--- Sources/NIOPostgres/Message/PostgresMessageEncoder.swift
+++ Sources/NIOPostgres/Message/PostgresMessageEncoder.swift
@@ -3,34 +3,28 @@ import NIO
 public final class PostgresMessageEncoder: MessageToByteEncoder {
     /// See `MessageToByteEncoder`.
     public typealias OutboundIn = PostgresMessage
-    
+
     /// See `MessageToByteEncoder`.
     public func encode(data message: PostgresMessage, out: inout ByteBuffer) throws {
         // print("PostgresMessage.ChannelEncoder.encode(\(data))")
-        
+
         // serialize identifier
         var message = message
         switch message.identifier {
         case .none: break
-        case .sslUnsupported, .sslSupported:
-            out.write(identifier: message.identifier)
-            return
         default:
             out.write(identifier: message.identifier)
         }
-        
+
         // leave room for identifier and size
         let messageSizeIndex = out.writerIndex
         out.moveWriterIndex(forwardBy: 4)
-        
+
         // serialize the message data
         out.writeBuffer(&message.data)
-        
+
         // set message size
         out.setInteger(Int32(out.writerIndex - messageSizeIndex), at: messageSizeIndex)
-    }
-    public init() {
-        
     }
 }
 
